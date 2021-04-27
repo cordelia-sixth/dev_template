000ca65c988fb36de7d0a62b411130650fad5463
cordelia <cordeliasixth@gmail.com>
2021-04-28 04:22:25 +0900

    Revert "finish"
    This reverts commit 9f17d9955750b2678d6f828d63c287a99e5d3655.

--- rails/app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-
-  # uploaderと紐づける。
-  # これでActiveRecordはAvatarUploaderの設定を利用した上で
-  # avatarカラムを操作できる。
-  # さらにCarrierWave::Uploader::Baseのメソッドも使える。
-  mount_uploader :avatar, AvatarUploader
-end
--- rails/app/views/users/_user.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! user, :id, :nickname, :age, :created_at, :updated_at
-json.url user_url(user, format: :json)
